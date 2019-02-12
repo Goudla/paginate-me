0aa81fa8a171c94301a79f0d8d98aa9af5dc8bfb
Douglas Bamber <douglasbamber@gmail.com>
2019-02-13 12:56:37 +1300

    ADD document main function

--- index.js
+++ index.js
@@ -7,6 +7,13 @@
     this[name] = definition();
   }
 }(' paginateMe', function() {
+  /**
+   * Returns a relivant slice of a collection
+   * @param  {Array<Object>} collection The collection being paginated.
+   * @param  {number} page The page number
+   * @param  {number} count The amount of values per page
+   * @return {[Array<Object>} The relivant slice of the collection
+   */
   return function(collection, page, count) {
     var begin = (page - 1) * count;
     var end = page * count;
